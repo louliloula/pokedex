b15e5dc76ff8467d90aab5568a02defc2093a7d8
loula <wmhadjou@gmail.com>
2023-10-07 20:03:54 +0200

    Fichier test

---
+++ lib/testEvolutions.dart
@@ -0,0 +1,24 @@
+//Recuperer la liste de mon json
+//loadPokemon
+//Creer une liste vide qui representerai les premieres evolutions
+//List<Evolutions> firstEvolutionsList = [];
+//Iterer dans la liste de mon json
+//for(var pokemon in loadPokemon){
+//List<Evolution> premiereEvolution = pokemon.nomdemafonction
+//premiereEvolution.add(premiereEvolution)
+// }
+
+
+
+//Creer une fonction pour recuperer les premieres evolutions de pokemons
+//List<Evolutions> premiereEvolutions(){
+// List<Evolutions> firstEvolutionsList = [];
+// Iterer dans ma liste d'evolutions
+//for(var evolution in evolutions!){
+// if(evolution.level == 1){
+//
+//ajouter les premieres evolutions dans la liste
+// premiereEvolutions.add(evolution)
+// }
+//
+//return premiereEvolutions
